b20ca346becee0572df7abd28d635406ac40e7bd
John Azzaro <jazzaro@temple.edu>
2020-04-03 19:50:08 -0700

    Fixed error with state folder

--- test-event-handlers/src/App.js
+++ test-event-handlers/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Tabs from './state/Tabs';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
 }
 
 export default App;
+
--- test-event-handlers/src/Tabs.test.js
+++ test-event-handlers/src/Tabs.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tabs from './Tabs';
+import Tabs from './state/Tabs';
 
 describe(`Tabs Component`, () => {
   it('renders without errors', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Tabs />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    ReactDOM.unmountComponentAtNode(div);  
   })
 })
