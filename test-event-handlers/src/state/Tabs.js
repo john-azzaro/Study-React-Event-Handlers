566217a40682184a44993af73d2ef404ee57ed43
John Azzaro <jazzaro@temple.edu>
2020-04-04 22:17:53 -0700

    reactored code

--- test-event-handlers/src/state/Tabs.js
+++ test-event-handlers/src/state/Tabs.js
@@ -2,21 +2,26 @@ import React from "react";
 
 class Tabs extends React.Component {
   static defaultProps = { tabs: [] };
-  render() { 
-    const buttons = this.props.tabs.map((tab, index) => (
+  renderButtons() {
+    return this.props.tabs.map((tab, index) => (
       <button key={index}>
         {tab.name}
       </button>
-    ));
-    const currentTab = this.props.tabs[0];
+    ))
+  }
+  renderContent() {
+    const currentTab = this.props.tabs[0]
+    return (
+      <div className='content'>
+        {currentTab.content}
+      </div>
+    )
+  } 
+  render() { 
     return (
       <div>
-        {buttons}
-        {this.props.tabs.length && (
-          <div className='content'>
-            {currentTab.content}
-          </div>
-        )}
+        {this.renderButtons()}
+        {this.props.tabs.length && this.renderContent()}
       </div>
     )
   }
